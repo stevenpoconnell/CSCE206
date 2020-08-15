ff908e2bb4f55e4ae6556efecef35e82308ead98
stevenpoconnell <69654841+stevenpoconnell@users.noreply.github.com>
2020-08-15 13:42:07 -0500

    Add files via upload

---
+++ Assignment 4/Hw4_q1_code.c
@@ -0,0 +1,46 @@
+/* Hw4_q1_code.c
+Steven O'Connell
+UIN: 128002247
+CSCE 206 - Section 502 */
+
+#include <stdio.h>
+int main()
+{
+
+    char str[256], rev[256];
+    int i, j, last, length = 0;
+    
+    printf("\nPlease type in the characters: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        } else {
+            if(str [i] >= 'A' && str[i] <= 'Z')
+            {
+            str[i]=str[i]+32;
+            }
+        }
+    }
+    
+    printf("Convert little letters to big/big letters to little: %s",str);
+
+    while (str[length] != '\0')
+        length ++;
+        last = length - 1;
+
+    for (j = 0; j < length; j++)
+    {
+        rev[j] = str[last];
+        last --;
+    }
+    
+    rev[j] ='\0';
+    
+    printf("\nReverse the converted characters: %s \n\n",rev);
+    
+    return 0;
+}
---
+++ Assignment 4/Hw4_q2_code.c
@@ -0,0 +1,37 @@
+/* Hw4_q2_code.c
+Steven O'Connell
+UIN: 128002247
+CSCE 206 - Section 502 */
+
+#include <stdio.h>
+
+int get_value(char ch) {
+    if (ch >= '0' && ch <= '9') {
+        return ch - '0';
+    } else {
+        if (ch >= 'A' && ch <= 'F') {
+            return 10 + (ch - 'A');
+        }
+        return 10 + (ch - 'a');
+    }
+}
+
+int main()
+{
+    long value = 0;
+    int i = 0;
+    char str[100];
+    
+    printf("Please input a hex number: ");
+    scanf("%s", str);
+    
+    while (str[i] != '\0') {
+        value = value * 16;
+        value += get_value(str[i]);
+        i++;
+    }
+   
+    printf("Its corresponding decimal is %ld \n", value);
+    
+    return 0;
+}
